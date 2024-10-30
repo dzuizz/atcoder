ee1891f324b0094f7883e2fc404c51a0229206f1
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2024-10-30 18:58:30 +0800

    Solved ABC 375B - Traveling Takahashi Problem

--- B-Traveling_Takahashi_Problem.cpp
+++ B-Traveling_Takahashi_Problem.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-using namespace std;
 
-#define int long long
+int main() {
+  int n; std::cin >> n;
 
-signed main() {
-  int n, x=0, y=0; cin >> n;
-  double ans = 0;
-  for (int i=0, a, b; i<n; ++i) {
-    cin >> a >> b;
-    ans += sqrt((a-x)*(a-x) + (b-y)*(b-y));
-    x = a, y = b;
+  int x=0, y=0, a, b;
+  double ans=0;
+  for (size_t i=0; i<n; ++i) {
+    std::cin >> a >> b;
+    ans += sqrt((long long)(a-x)*(a-x) + (long long)(b-y)*(b-y));
+    x=a, y=b;
   }
-  ans += sqrt(x*x + y*y);  
-  cout << fixed << setprecision(6) << ans << '\n';
+  ans += sqrt((long long)x*x + (long long)y*y);
+  std::cout << std::fixed << std::setprecision(6) << ans << '\n';
 }
