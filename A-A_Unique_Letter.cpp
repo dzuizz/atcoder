f09d8465dad6a46ee71bed032c8e580129a223d8
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2024-10-30 18:52:02 +0800

    Solved ABC 260A - A Unique Letter

--- A-A_Unique_Letter.cpp
+++ A-A_Unique_Letter.cpp
@@ -1,11 +1,12 @@
+// ABC 260
+
 #include <iostream>
-using namespace std;
-typedef long long ll;
 
 int main() {
-    string s; cin >> s;
-    if (s[0] != s[1] && s[0] != s[2]) cout << s[0] << '\n';
-    else if (s[1] != s[0] && s[1] != s[2]) cout << s[1] << '\n';
-    else if (s[2] != s[0] && s[2] != s[1]) cout << s[2] << '\n';
-    else cout << "-1\n";
+  std::string s; std::cin >> s;
+  if (s[0] != s[1] && s[0] != s[2]) std::cout << s[0] << '\n';
+  else if (s[1] != s[0] && s[1] != s[2]) std::cout << s[1] << '\n';
+  else if (s[2] != s[0] && s[2] != s[1]) std::cout << s[2] << '\n';
+  else std::cout << "-1\n";
 }
+
