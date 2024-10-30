ec3e9fd5787d2d3ff37b4702c9dd3840b2bbd3c5
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2024-10-30 16:47:27 +0800

    Solved ABC 375A - Seats

---
+++ A-Seats.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int n, cnt=0; std::cin >> n;
+  std::string s; std::cin >> s;
+  for (int i=0; i<n-2; ++i) {
+    if (s[i] == '#' && s[i+1] == '.' && s[i+2] == '#') ++cnt;
+  }
+  std::cout << cnt << '\n';
+}
