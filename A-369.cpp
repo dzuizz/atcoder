482dee1763ff47a83a564bc55a55e55c098a22ab
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2024-10-30 18:46:14 +0800

    Solved ABC 369A - 369

--- A-369.cpp
+++ A-369.cpp
@@ -1,12 +1,9 @@
 // ABC 369
 
 #include <iostream>
-using namespace std;
 
 int main() {
-  int A, B; cin >> A >> B;
-  if (A == B) cout << "1\n";
-  else if ((A-B)%2) cout << "2\n";
-  else cout << "3\n";
+  int A, B; std::cin >> A >> B;
+  std::cout << (A == B ? 1 : A&1^(B&1) ? 2 : 3) << '\n';
 }
 
