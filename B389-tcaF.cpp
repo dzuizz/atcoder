314eb827774806ebc5b1524d3846ea9a3487f131
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-02-07 19:27:06 +0800

    Solved ABC 389B - tcaF

---
+++ B389-tcaF.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+  int N=1,f=1,X; cin>>X;
+  while(f!=X) f*=++N;
+  cout<<N<<'\n';
+}
+signed main(){
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  int t=1;
+  // cin>>t;
+  for(int i=1;i<=t;++i){
+    //cout<<"Case #"<<i<<": ";
+    solve();
+  }
+  return 0;
+}
+
