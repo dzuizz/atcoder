2551423bc9172acf4aef9770147d1e40748ff190
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-02-07 19:26:44 +0800

    Solved ABC 389A - 9x9

---
+++ A389-9x9.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+  string s; cin>>s;
+  cout<<(s[0]-'0')*(s[2]-'0')<<'\n';
+}
+signed main(){
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  int t=1;
+  // cin>>t;
+  for(int i=1;i<=t;++i){
+    //cout<<"Case #"<<i<<": ";
+    solve();
+  }
+  return 0;
+}
+
