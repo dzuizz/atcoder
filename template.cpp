703d0828fc9282e2c0ce564be8ad1587f9b3c008
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-02-09 22:56:01 +0800

    Template for AtCoder solutions

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+  
+}
+signed main(){
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  int t=1;
+  // cin>>t;
+  for(int i=1;i<=t;++i){
+    //cout<<"Case #"<<i<<": ";
+    solve();
+  }
+  return 0;
+}
