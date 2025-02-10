8aa4692eef9f03822e213bdfefadce59b0fca9c2
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-02-11 07:18:33 +0800

    Solved ABC 384A - aaadaa

---
+++ BA384-aaaadaa.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+  int N;
+  char a,b;
+  cin>>N>>a>>b;
+  string S; cin>>S;
+  for(auto&c:S)
+    cout<<(c==a?c:b);
+  cout<<'\n';
+}
+signed main(){
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  int t=1;
+  // cin>>t;
+  for(int i=1;i<=t;++i){
+    //cout<<"Case #"<<i<<": ";
+    solve();
+  }
+  return 0;
+}
+
