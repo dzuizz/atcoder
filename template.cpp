cff3b6e2bea6e2c935a6c1326091fba78bfebf5c
Ahmad Dzuizz Annajib <dzuizz@m4-air---dzuizz.local>
2025-10-11 21:57:59 +0800

    Update template file - v1

--- template.cpp
+++ template.cpp
@@ -1,16 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-void solve(){
-  
-}
 signed main(){
-  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  int t=1;
-  // cin>>t;
-  for(int i=1;i<=t;++i){
-    //cout<<"Case #"<<i<<": ";
-    solve();
-  }
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  
   return 0;
 }
