888e28fbae8fe2a8877d0ada43e6f09c2518d90b
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2026-08-18 15:45:08 +0800

    abc454a

---
+++ abc454/a.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main(){
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int l,r; cin>>l>>r;
+  cout<<r-l+1<<'\n';
+  return 0;
+}
